42db94d1d0bab9aeb4ad2e00369f9671d1353f5a
MakhtarNdiaye <bintabocoum@Bintas-MacBook-Pro.local>
2019-01-04 09:33:52 -0500

    fixed a bug

---
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,30 @@
+//
+//  LeagueVC.swift
+//  app-swoosh
+//
+//  Created by MakhtarNdiaye on 1/4/19.
+//  Copyright © 2019 MakhtarNdiaye. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- app-swoosh/app-swoosh/Controller/WelcomeVC.swift
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -8,11 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeVC: UIViewController {
 
-    @IBOutlet weak var swoosh: UIImageView!
-    @IBOutlet weak var bgImg: UIImageView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
